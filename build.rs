e5adc2adbe64c3c94c0e073601704aa28974ae68
Dan Burkert <dan@danburkert.com>
2021-03-03 15:22:23 -0800

    use bytes

--- build.rs
+++ build.rs
@@ -1,5 +1,9 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure().format(false).compile(
+    let mut config = prost_build::Config::new();
+    config.bytes(&["."]);
+
+    tonic_build::configure().format(false).compile_with_config(
+        config,
         &[
             "googleapis/google/cloud/bigquery/storage/v1/arrow.proto",
             "googleapis/google/cloud/bigquery/storage/v1/avro.proto",
